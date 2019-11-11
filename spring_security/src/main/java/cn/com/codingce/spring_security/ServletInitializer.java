3b77134638e214f27dd0796a31c205bb1a52a1a0
MaXinze <2460798168@qq.com>
2019-11-11 15:38:42 +0800

    A Spring Security原理篇(一) 启动原理

---
+++ spring_security/src/main/java/cn/com/codingce/spring_security/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cn.com.codingce.spring_security;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringSecurityApplication.class);
+    }
+
+}
---
+++ spring_security/src/test/java/cn/com/codingce/spring_security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.codingce.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
