5da3e16061b27bf890b07dceba59127af3f415df
MaXinze <2460798168@qq.com>
2020-07-21 14:05:31 +0800

    A 依赖注入方式

--- mySpring/spring-04-di/src/main/java/cn/com/codingce/pojo/Student.java
+++ mySpring/spring-04-di/src/main/java/cn/com/codingce/pojo/Student.java
@@ -99,7 +99,7 @@ public class Student {
     public String toString() {
         return "Student{" +
                 "name='" + name + '\'' +
-                ", address=" + address +
+                ", address=" + address.toString() +
                 ", books=" + Arrays.toString(books) +
                 ", hobbys=" + hobbys +
                 ", card=" + card +
--- mySpring/spring-04-di/src/test/java/MyTest.java
+++ mySpring/spring-04-di/src/test/java/MyTest.java
@@ -1,10 +1,26 @@
+import cn.com.codingce.pojo.Student;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
 
     @Test
     public void Test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.getName());
 
+        System.out.println("=================================");
+        System.out.println(student.toString());
+
+        /**
+         *Student{name='掌上编程', address=Address{address='天津市西青区天津理工大学'},
+         * books=[红楼梦, 西游记, 水浒传, 三国演义], hobbys=[听音乐, 敲代码, 看电影],
+         * card={身份证=1111111111111112, 银行卡=11111111111, 信用卡=11111111111111},
+         * games=[GTA5, 彩虹6号, LOL], wife='null',
+         * info={学号=20200521, 性别=男, 姓名=掌上编程}}
+         **/
     }
 
 }
