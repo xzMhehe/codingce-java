bd4720f6cba9d5990455ed0dc3b431207aec12e8
MaXinze <2460798168@qq.com>
2020-07-29 09:57:10 +0800

    A 代理模式

---
+++ mySpring/spring-08-proxy/src/main/java/cn/com/codingce/demo01/Host.java
@@ -0,0 +1,12 @@
+package cn.com.codingce.demo01;
+
+/**
+ * @author xzMa
+ * 房东
+ *
+ */
+public class Host implements Rent {
+    public void rent() {
+        System.out.println("房东要出租房子");
+    }
+}
---
+++ mySpring/spring-08-proxy/src/main/java/cn/com/codingce/demo01/Rent.java
@@ -0,0 +1,11 @@
+package cn.com.codingce.demo01;
+
+/**
+ * @author xzMa
+ * 租房
+ */
+public interface Rent {
+
+    public void rent();
+
+}
