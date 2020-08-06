6336be5519aed37896cb871921f762df35e5a7d3
MaXinze <2460798168@qq.com>
2020-08-07 07:11:33 +0800

    A Ajax验证用户名体验

--- SpringMVC/springmvc-07-ajax/src/main/java/cn/com/codingce/controller/AjaxController.java
+++ SpringMVC/springmvc-07-ajax/src/main/java/cn/com/codingce/controller/AjaxController.java
@@ -41,4 +41,25 @@ public class AjaxController {
         return list;
     }
 
+    @RequestMapping("/a3")
+    public String ajax3(String name,String pwd){
+        String msg = "";
+        //模拟数据库中存在数据
+        if (name!=null){
+            if ("admin".equals(name)){
+                msg = "OK";
+            }else {
+                msg = "用户名输入错误";
+            }
+        }
+        if (pwd!=null){
+            if ("123456".equals(pwd)){
+                msg = "OK";
+            }else {
+                msg = "密码输入有误";
+            }
+        }
+        return msg; //由于@RestController注解，将msg转成json格式返回
+    }
+
 }
