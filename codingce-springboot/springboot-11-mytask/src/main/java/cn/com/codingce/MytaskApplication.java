224bdb52622c61ab2882b7b0bb50a45d1df6520f
MaXinze <2460798168@qq.com>
2020-08-24 13:37:16 +0800

    A 邮件发送

--- codingce-springboot/springboot-11-mytask/src/main/java/cn/com/codingce/MytaskApplication.java
+++ codingce-springboot/springboot-11-mytask/src/main/java/cn/com/codingce/MytaskApplication.java
@@ -2,7 +2,9 @@ package cn.com.codingce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync    //开启异步注解功能
 @SpringBootApplication
 public class MytaskApplication {
 
