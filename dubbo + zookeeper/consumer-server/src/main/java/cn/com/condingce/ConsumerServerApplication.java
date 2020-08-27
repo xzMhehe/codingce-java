1c8dcb7614e7f5da40088c3a87a1d4ae24c1850f
MaXinze <2460798168@qq.com>
2020-08-27 09:50:32 +0800

    A dubbo&zookeeper 环境搭建

---
+++ dubbo + zookeeper/consumer-server/src/main/java/cn/com/condingce/ConsumerServerApplication.java
@@ -0,0 +1,13 @@
+package cn.com.condingce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerServerApplication.class, args);
+	}
+
+}
---
+++ dubbo + zookeeper/consumer-server/src/test/java/cn/com/condingce/ConsumerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.condingce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
