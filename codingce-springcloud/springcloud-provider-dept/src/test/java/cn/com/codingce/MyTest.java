baf20421fb508f602fddf9a12434ae28b449441a
MaXinze <2460798168@qq.com>
2020-09-07 23:22:14 +0800

    F 测试无误

--- codingce-springcloud/springcloud-provider-dept/src/test/java/cn/com/codingce/MyTest.java
+++ codingce-springcloud/springcloud-provider-dept/src/test/java/cn/com/codingce/MyTest.java
@@ -1,5 +1,20 @@
 package cn.com.codingce;
 
 
+import cn.com.codingce.service.DeptService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class MyTest {
+
+    @Autowired
+    private DeptService deptService;
+
+    @Test
+    public void mytest() {
+        System.out.println(deptService.queryAll());
+    }
+
 }
