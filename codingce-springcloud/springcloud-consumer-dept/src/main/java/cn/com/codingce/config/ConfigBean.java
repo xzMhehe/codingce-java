6b524de886f0ba06dcc6fbde6dd35fc69b51e6c3
MaXinze <2460798168@qq.com>
2020-09-07 23:40:17 +0800

    A 消费者环境搭建

---
+++ codingce-springcloud/springcloud-consumer-dept/src/main/java/cn/com/codingce/config/ConfigBean.java
@@ -0,0 +1,8 @@
+package cn.com.codingce.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigBean {   //configuration -- spring applicationContext.xml
+
+}
---
+++ codingce-springcloud/springcloud-consumer-dept/src/main/java/cn/com/codingce/controller/DeptConsumerController.java
@@ -0,0 +1,12 @@
+package cn.com.codingce.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DeptConsumerController {
+
+    // 理解消费者, 不应该有service层
+    //RestFul风格
+
+
+}
