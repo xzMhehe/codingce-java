b165f49af7bd861b0555d6bc2e6606858aad54be
MaXinze <2460798168@qq.com>
2020-09-10 22:27:10 +0800

    A 服务注册与集群搭建

--- codingce-springcloud/springcloud-provider-dept/src/main/java/cn/com/codingce/DeptProvider.java
+++ codingce-springcloud/springcloud-provider-dept/src/main/java/cn/com/codingce/DeptProvider.java
@@ -3,6 +3,7 @@ package cn.com.codingce;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient //开启Eureka 在服务启动后, 自动注册到Eureka中
+@EnableDiscoveryClient  //服务发现
 public class DeptProvider {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider.class, args);
