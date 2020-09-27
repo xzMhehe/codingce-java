1c0ad07e00389993f8184a7e384d0a8cf535b57f
MaXinze <2460798168@qq.com>
2020-09-27 15:08:16 +0800

    A springcloud-config服务端实现

---
+++ codingce-springcloud/springcloud-config-server-3344/src/main/java/cn/com/codingce/springcloud/Config_Server_3344.java
@@ -0,0 +1,13 @@
+package cn.com.codingce.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Config_Server_3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Server_3344.class, args);
+    }
+}
