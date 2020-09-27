9912415c50cc92ee04c9eb053c5ca86a80aca9af
MaXinze <2460798168@qq.com>
2020-09-27 15:59:31 +0800

    A springcloud-config客户端实现

---
+++ codingce-springcloud/springcloud-config-client-3355/src/main/java/cn/com/codingce/springcloud/Config_Client_3355.java
@@ -0,0 +1,11 @@
+package cn.com.codingce.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Config_Client_3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Client_3355.class, args);
+    }
+}
