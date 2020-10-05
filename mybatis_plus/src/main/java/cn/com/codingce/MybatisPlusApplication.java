5406381bb987cdabaf3e88a0df792aa28abebb23
MaXinze <2460798168@qq.com>
2020-10-05 10:46:52 +0800

    A mybatis_plus环境搭建

---
+++ mybatis_plus/src/main/java/cn/com/codingce/MybatisPlusApplication.java
@@ -0,0 +1,16 @@
+package cn.com.codingce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xzMa
+ */
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+}
