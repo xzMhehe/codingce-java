ad3d0b5b9a6daf63e75cc0c851ed4307765b4f0b
Ma Xinze <980625>
2021-01-08 19:34:24 +0800

    A jedis基本方法

--- codingce-redis/redis-01-jedis/src/test/java/cn/com/codincge/RedisApplicationTests.java
+++ codingce-redis/redis-01-jedis/src/test/java/cn/com/codincge/RedisApplicationTests.java
@@ -3,20 +3,38 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import redis.clients.jedis.Jedis;
 
+import java.util.Set;
+
 @SpringBootTest
 class RedisApplicationTests {
-
     @Test
     void TestPing() {
         //1 new Jedis 对象即可
         Jedis jedis = new Jedis("127.0.0.1", 6379);
         //jedis 所有的命令就是他的基本命令, 就是对象的方法
-        System.out.println(jedis.ping());;
+        System.out.println(jedis.ping());
     }
 
     @Test
     void TestKey() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        System.out.println("清空数据" + jedis.flushDB());
+        System.out.println("判断某个键是否存在: " + jedis.exists("username"));
+        System.out.println("新增'<username, mxz>的键值对'" + jedis.set("username", "mxz"));
+        System.out.println("新增'<password, mxz>的键值对'" + jedis.set("password", "mxz"));
+        System.out.println("系统中所有的键值对如下: ");
+        Set<String> keys = jedis.keys("*");
+        System.out.println(keys);
 
+        System.out.println("删除键password: " + jedis.del("password"));
+        System.out.println("判断键password是否存在: " + jedis.exists("password"));
+        System.out.println("查看键username所存储值的类型: " + jedis.type("username"));
+        System.out.println("随机返回key空间的一个: " + jedis.randomKey());
+        System.out.println("重命名key: " + jedis.rename("username", "name"));
+        System.out.println("按索引查询: " + jedis.select(0));
+        System.out.println("删除当前选择数据库的所有的key: " + jedis.flushDB());
+        System.out.println("返回当前数据库中key的数目: " + jedis.dbSize());
+        System.out.println("删除所有数据库中的所有的key: " + jedis.flushAll());
     }
 
 }
