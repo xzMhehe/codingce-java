081ef510adaa9be8efa9d2d79e847b24eb540c60
Ma Xinze <980625>
2021-01-08 16:16:48 +0800

    A Jedis TestPing

--- codingce-redis/redis-01-jedis/src/test/java/cn/com/codincge/RedisApplicationTests.java
+++ codingce-redis/redis-01-jedis/src/test/java/cn/com/codincge/RedisApplicationTests.java
@@ -1,13 +1,22 @@
 package cn.com.codincge;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
 
 @SpringBootTest
 class RedisApplicationTests {
 
     @Test
-    void contextLoads() {
+    void TestPing() {
+        //1 new Jedis 对象即可
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        //jedis 所有的命令就是他的基本命令, 就是对象的方法
+        System.out.println(jedis.ping());;
+    }
+
+    @Test
+    void TestKey() {
+
     }
 
 }
