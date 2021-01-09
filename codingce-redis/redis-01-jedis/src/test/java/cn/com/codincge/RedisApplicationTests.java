01a9c80c0cb076d8ce1b13ff2deb78ffc311b6b9
Ma Xinze <980625>
2021-01-09 09:59:21 +0800

    A Jedis String

--- codingce-redis/redis-01-jedis/src/test/java/cn/com/codincge/RedisApplicationTests.java
+++ codingce-redis/redis-01-jedis/src/test/java/cn/com/codincge/RedisApplicationTests.java
@@ -1,9 +1,11 @@
 package cn.com.codincge;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import redis.clients.jedis.Jedis;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class RedisApplicationTests {
@@ -35,6 +37,51 @@ class RedisApplicationTests {
         System.out.println("删除当前选择数据库的所有的key: " + jedis.flushDB());
         System.out.println("返回当前数据库中key的数目: " + jedis.dbSize());
         System.out.println("删除所有数据库中的所有的key: " + jedis.flushAll());
+    }
+
+    @Test
+    void TestString() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        System.out.println("=====增加数据=====");
+        System.out.println(jedis.set("key1", "value1"));
+        System.out.println(jedis.set("key2", "value2"));
+        System.out.println(jedis.set("key3", "value3"));
+        System.out.println("删除键key2" + jedis.del("key2"));
+        System.out.println("获取键key2" + jedis.get("key2"));
+        System.out.println("修改key1" + jedis.set("key1", "valueChanged"));
+        System.out.println("获取key1的值" + jedis.get("key1"));
+        System.out.println("在key3后面加入值" + jedis.append("key3", "end"));
+        System.out.println("key3的值" + jedis.get("key3"));
+        System.out.println("增加多个键值对: " + jedis.mset("key01", "value01", "key02", "value02"));
+        System.out.println("获取多个键值对: " + jedis.mget("key01", "key02", "key03"));
+        System.out.println("获取多个键值对: " + jedis.mget("key01", "key02", "key03", "key04"));
+        System.out.println("删除多个键值对: " + jedis.del("key01", "key02"));
+        System.out.println("获取多个键值对: " + jedis.mget("key01", "key02", "key03"));
+
+        jedis.flushDB();
+        System.out.println("=====新增键值对防止覆盖原先值=====");
+        System.out.println(jedis.setnx("key1", "value1"));
+        System.out.println(jedis.setnx("key2", "value2"));
+        System.out.println(jedis.setnx("key2", "value2-new"));
+        System.out.println(jedis.get("key1"));
+        System.out.println(jedis.get("key2"));
+
+        System.out.println("=====新增键值对并设置有效时间=====");
+        System.out.println(jedis.setex("key3", 2, "value3"));
+        System.out.println(jedis.get("key3"));
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(jedis.get("key3"));
+
+
+        System.out.println("=====获取原值, 更新为新值=====");
+        System.out.println(jedis.getSet("key2", "key2GetSet"));
+        System.out.println(jedis.get("key2"));
+
+        System.out.println("获得key2的值字符串: " + jedis.getrange("key2", 2, 4));
     }
 
 }
