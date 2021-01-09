4eb5404d701fabb939f1724b0dce20ec05e9244e
Ma Xinze <980625>
2021-01-09 11:32:40 +0800

    A Jedis Set

--- codingce-redis/redis-01-jedis/src/test/java/cn/com/codincge/RedisApplicationTests.java
+++ codingce-redis/redis-01-jedis/src/test/java/cn/com/codincge/RedisApplicationTests.java
@@ -122,4 +122,46 @@ class RedisApplicationTests {
         System.out.println("sortedList排序后：" + jedis.lrange("sortedList", 0, -1));
     }
 
+    @Test
+    void TestSet() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        jedis.flushDB();
+        System.out.println("======向集合中添加元素(不重复)======");
+        System.out.println(jedis.sadd("eleSet", "e0", "e1", "e2", "e3", "e4", "e5"));
+        System.out.println(jedis.sadd("eleSet", "e6"));
+        System.out.println(jedis.sadd("eleSet", "e6"));
+        System.out.println("eleSet的所有元素为: " + jedis.smembers("eleSet"));
+        System.out.println("删除一个元素e0" + jedis.srem("eleSet", "e0"));
+        System.out.println("删除两个元素e6 e7" + jedis.srem("eleSet", "e7", "e6"));
+        System.out.println("eleSet的所有元素为: " + jedis.smembers("eleSet"));
+        System.out.println("随机移除集合中的一个元素: " + jedis.spop("eleSet"));
+        System.out.println("随机移除集合中的一个元素: " + jedis.spop("eleSet"));
+        System.out.println("eleSet的所有元素为: " + jedis.smembers("eleSet"));
+        System.out.println("eleSet中包含元素的个数: " + jedis.scard("eleSet"));
+        System.out.println("e3是否在eleSet中: " + jedis.sismember("eleSet", "e3"));
+        System.out.println("e1是否在eleSet中: " + jedis.sismember("eleSet", "e1"));
+        System.out.println("e5是否在eleSet中: " + jedis.sismember("eleSet", "e5"));
+        System.out.println("========================================");
+        System.out.println(jedis.sadd("eleSet1", "e0", "e1", "e2", "e3", "e4", "e5"));
+        System.out.println(jedis.sadd("eleSet2", "e0", "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8"));
+        System.out.println("将eleSet1中删除e1并存入eleSet3中: " + jedis.smove("eleSet1", "eleSet3", "e1"));
+        System.out.println("将eleSet2中删除e2并存入eleSet3中: " + jedis.smove("eleSet2", "eleSet3", "e2"));
+        System.out.println("eleSet1中的元素: " + jedis.smembers("eleSet1"));
+        System.out.println("eleSet2中的元素: " + jedis.smembers("eleSet2"));
+        System.out.println("=====集合运算=====");
+        System.out.println("eleSet1中的元素: " + jedis.smembers("eleSet1"));
+        System.out.println("eleSet2中的元素: " + jedis.smembers("eleSet2"));
+        System.out.println("eleSet1和eleSet2的交集" + jedis.sinter("eleSet1", "eleSet2"));
+        System.out.println("eleSet1和eleSet2的并集" + jedis.sunion("eleSet1", "eleSet2"));
+        System.out.println("eleSet1和eleSet2的差集" + jedis.sdiff("eleSet1", "eleSet2")); //eleSet1中有, EleSet2中没有
+        jedis.sinterstore("eleSet4", "eleSet1", "eleSet2"); //求交集并将交集保存到 dstkey的集合
+        System.out.println("eleSet4中的元素: " + jedis.smembers("eleSet4"));
+    }
+
+    @Test
+    void TestHash() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        jedis.flushDB();
+    }
+
 }
