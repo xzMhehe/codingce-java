07dee11024c4f14e027f49c283426de87a36390a
Ma Xinze <2460798168@qq.com>
2021-01-19 17:34:36 +0800

    A 索引的增删查;

--- codingce-es/codingce-es-api/src/test/java/cn/com/codingce/CodingceEsApiApplicationTests.java
+++ codingce-es/codingce-es-api/src/test/java/cn/com/codingce/CodingceEsApiApplicationTests.java
@@ -1,13 +1,53 @@
 package cn.com.codingce;
 
+import cn.com.codingce.config.ElasticSearchConfig;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class CodingceEsApiApplicationTests {
 
+    @Autowired
+    @Qualifier("restHighLevelClient")
+    private RestHighLevelClient client;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        // 创建索引
+        CreateIndexRequest request = new CreateIndexRequest("codingce_index");
+        // 执行创建请求, IndicesClient, 请求后获得响应
+        CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
+        System.out.println(createIndexResponse);
+    }
+
+    //测试获取索引
+    @Test
+    void testExistIndex() throws IOException {
+        GetIndexRequest request = new GetIndexRequest("codingce_index");
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        System.out.println(exists);
+
+    }
+
+
+    //测试删除索引
+    @Test
+    void testDeleteIndex() throws IOException {
+        DeleteIndexRequest request = new DeleteIndexRequest("codingce_index");
+        AcknowledgedResponse delete = client.indices().delete(request, RequestOptions.DEFAULT);
+        System.out.println(delete.isAcknowledged());
+
     }
 
 }
