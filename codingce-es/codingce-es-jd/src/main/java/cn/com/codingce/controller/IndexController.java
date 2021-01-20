48bdb37eed150264f296e3ea55d829650e4b122d
Ma Xinze <2460798168@qq.com>
2021-01-20 16:01:48 +0800

    A 京东搜索环境搭建

---
+++ codingce-es/codingce-es-jd/src/main/java/cn/com/codingce/controller/IndexController.java
@@ -0,0 +1,18 @@
+package cn.com.codingce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author mxz
+ */
+@Controller
+public class IndexController {
+
+
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ codingce-es/codingce-es-jd/src/main/java/cn/com/codingce/utils/HtmlParseUtil.java
@@ -0,0 +1,73 @@
+package cn.com.codingce.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HtmlParseUtil {
+    public static void main(String[] args) throws IOException {
+
+        // 获取请求 https://search.jd.com/Search?keyword=java
+        String url = "https://search.jd.com/Search?keyword=java";
+
+        // 解析网页 (返回 Document 就是浏览器 Document 对象)
+        Document document = Jsoup.parse(new URL(url), 30000);
+
+        // 所有在js中可以使用的方法, 这里都可以使用
+        Element element = document.getElementById("J_goodsList");
+//        System.out.println(element.html());
+
+        // 获取所有的li元素
+        Elements elements = element.getElementsByTag("li");
+        // 获取元素中的内容
+        for (Element el : elements) {
+            // 关于这种图片特别多的网站, 所有的图片都是延迟加载的
+            String img = el.getElementsByTag("img").eq(0).attr("src");
+            String price = el.getElementsByClass("p-price").eq(0).text();
+            String title = el.getElementsByClass("p-name").eq(0).text();
+            System.out.println("=====================================");
+            System.out.println(img);
+            System.out.println(price);
+            System.out.println(title);
+        }
+    }
+
+
+
+    public List<Object> parseJS(String keywords) throws IOException {
+
+        // 获取请求 https://search.jd.com/Search?keyword=java
+        String url = "https://search.jd.com/Search?keyword=java";
+
+        // 解析网页 (返回 Document 就是浏览器 Document 对象)
+        Document document = Jsoup.parse(new URL(url), 30000);
+
+        // 所有在js中可以使用的方法, 这里都可以使用
+        Element element = document.getElementById("J_goodsList");
+//        System.out.println(element.html());
+
+        // 获取所有的li元素
+        Elements elements = element.getElementsByTag("li");
+        // 获取元素中的内容
+        for (Element el : elements) {
+            // 关于这种图片特别多的网站, 所有的图片都是延迟加载的
+            String img = el.getElementsByTag("img").eq(0).attr("src");
+            String price = el.getElementsByClass("p-price").eq(0).text();
+            String title = el.getElementsByClass("p-name").eq(0).text();
+            System.out.println("=====================================");
+            System.out.println(img);
+            System.out.println(price);
+            System.out.println(title);
+        }
+
+        return new ArrayList<>();
+
+    }
+}
