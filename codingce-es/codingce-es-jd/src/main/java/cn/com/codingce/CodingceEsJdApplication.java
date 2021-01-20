cde12e58491ce118f78f95ad7a24572b7f05a2f5
Ma Xinze <2460798168@qq.com>
2021-01-20 14:17:59 +0800

    A 京东搜索实战

---
+++ codingce-es/codingce-es-jd/src/main/java/cn/com/codingce/CodingceEsJdApplication.java
@@ -0,0 +1,16 @@
+package cn.com.codingce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mxz
+ */
+@SpringBootApplication
+public class CodingceEsJdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingceEsJdApplication.class, args);
+    }
+
+}
