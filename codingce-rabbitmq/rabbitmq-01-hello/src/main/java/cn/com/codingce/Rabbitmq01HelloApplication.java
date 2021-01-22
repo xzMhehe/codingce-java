a2b2407ec25a279f694b780c966fb4ffeb939c46
Ma Xinze <2460798168@qq.com>
2021-01-22 13:58:32 +0800

    A init rabbitmq-01-hello

---
+++ codingce-rabbitmq/rabbitmq-01-hello/src/main/java/cn/com/codingce/Rabbitmq01HelloApplication.java
@@ -0,0 +1,13 @@
+package cn.com.codingce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rabbitmq01HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rabbitmq01HelloApplication.class, args);
+    }
+
+}
