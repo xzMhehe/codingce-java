11e57aa381cbee07a3dba4ff3dcffeb45fb0880b
Ma Xinze <2460798168@qq.com>
2021-01-23 15:51:32 +0800

    A Routing 之订阅模型-Topic

---
+++ codingce-rabbitmq/rabbitmq-01-hello/src/main/java/cn/com/codingce/topic/CustomerTwo.java
@@ -0,0 +1,41 @@
+package cn.com.codingce.topic;
+
+import cn.com.codingce.utils.RabbitMQUtils;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+/**
+ * @author mxz
+ */
+public class CustomerTwo {
+
+    public static void main(String[] args) throws IOException {
+        Connection connection = RabbitMQUtils.getConnection();
+
+        Channel channel = connection.createChannel();
+
+        // 声明交换机以及交换机类型
+        channel.exchangeDeclare("topics", "topic");
+
+        // 创建一个临时队列
+        String queue = channel.queueDeclare().getQueue();
+
+        // 绑定队列和交换机  动态通配符  route key
+        channel.queueBind(queue, "topics", "user.#");
+
+        // 消费消息
+        channel.basicConsume(queue, true, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("消费者2：" + new String(body));
+            }
+        });
+
+    }
+
+}
---
+++ codingce-rabbitmq/rabbitmq-01-hello/src/main/java/cn/com/codingce/topic/Provider.java
@@ -0,0 +1,37 @@
+package cn.com.codingce.topic;
+
+import cn.com.codingce.utils.RabbitMQUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 生产者
+ * <p>
+ *
+ * @author mxz
+ */
+@Component
+public class Provider {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = RabbitMQUtils.getConnection();
+        Channel channel = connection.createChannel();
+
+
+        // 声明交换机以及交换机类型
+        channel.exchangeDeclare("topics", "topic");
+
+
+        // 路由key
+        String routeKey = "user.save";
+
+        channel.basicPublish("topics", routeKey, null, ("这里是 topic 动态路由模型, routeKey:[" + routeKey + "]").getBytes());
+
+        // 关闭资源
+        RabbitMQUtils.closeConnectionAndChannel(channel, connection);
+    }
+}
