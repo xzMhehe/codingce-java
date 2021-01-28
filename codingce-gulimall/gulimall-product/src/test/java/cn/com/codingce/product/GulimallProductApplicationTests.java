eca9930407f9682e7988b543fd5a458396ea20ff
Ma Xinze <2460798168@qq.com>
2021-01-28 14:09:38 +0800

    A 谷粒商城 init

---
+++ codingce-gulimall/gulimall-product/src/test/java/cn/com/codingce/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.codingce.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
