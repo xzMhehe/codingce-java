f980804bb7a8bfab6619a1fb869f611fee9c63be
Ma Xinze <2460798168@qq.com>
2021-01-30 20:06:00 +0800

    A 微服务的注册中心、微服务的配置中心

--- codingce-gulimall/gulimall-coupon/src/main/java/cn/com/codingce/coupon/GulimallCouponApplication.java
+++ codingce-gulimall/gulimall-coupon/src/main/java/cn/com/codingce/coupon/GulimallCouponApplication.java
@@ -5,6 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+ * 如何使用 nacos 作为配置中心, 统一管理配置
+ * 引入依赖
+ *         <dependency>
+ *             <groupId>com.alibaba.cloud</groupId>
+ *             <artifactId>spring-cloud-starter-alibaba-nacos-config</artifactId>
+ *         </dependency>
+ * 创建一个 bootstrap.proprties
+ *   spring.application.name=gulimall-coupon
+ *   # 指定 nacos 配置中心地址
+ *   spring.cloud.nacos.config.server-addr=127.0.0.1:8848
+ *
+ *
+ * @author mxz
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("cn.com.codingce.coupon.dao")
--- codingce-gulimall/gulimall-coupon/src/main/java/cn/com/codingce/coupon/controller/CouponController.java
+++ codingce-gulimall/gulimall-coupon/src/main/java/cn/com/codingce/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import cn.com.codingce.common.utils.R;
 import cn.com.codingce.coupon.entity.CouponEntity;
 import cn.com.codingce.coupon.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +20,30 @@ import java.util.Map;
 /**
  * 优惠券信息
  *
+ * nacos 自动刷新配置注解 @RefreshScope
+ *
  * @author mxz
  * @email codingce@gmail.com
  * @date 2021-01-29 16:27:44
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
 
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private int age;
+
     @Autowired
     private CouponService couponService;
+
+    @RequestMapping("/test")
+    public R test() {
+
+        return R.ok().put("name", name).put("age", age);
+    }
 
     /**
      * 测试远程调用 feign
