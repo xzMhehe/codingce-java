8fa47c9c33c0b6398fe825a5d24105f4a482f3f6
Ma Xinze <2460798168@qq.com>
2021-01-30 20:53:28 +0800

    A 路由网关springcloudgateway

---
+++ codingce-gulimall/gulimall-gateway/src/main/java/cn/com/codingce/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.com.codingce.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
