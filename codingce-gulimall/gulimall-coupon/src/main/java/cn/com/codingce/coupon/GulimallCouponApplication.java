92759a768b8d464a0d87fb3145c351b22f5dceea
Ma Xinze <2460798168@qq.com>
2021-01-30 16:29:44 +0800

    A 服务注册与发现 Nacos

--- codingce-gulimall/gulimall-coupon/src/main/java/cn/com/codingce/coupon/GulimallCouponApplication.java
+++ codingce-gulimall/gulimall-coupon/src/main/java/cn/com/codingce/coupon/GulimallCouponApplication.java
@@ -3,7 +3,9 @@ package cn.com.codingce.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("cn.com.codingce.coupon.dao")
 public class GulimallCouponApplication {
--- codingce-gulimall/gulimall-member/src/main/java/cn/com/codingce/member/GulimallMemberApplication.java
+++ codingce-gulimall/gulimall-member/src/main/java/cn/com/codingce/member/GulimallMemberApplication.java
@@ -1,9 +1,13 @@
 package cn.com.codingce.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("cn.com.codingce.member.dao")
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
