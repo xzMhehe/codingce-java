636b915df495ade821bc0064c2fdb8f4b7034a77
Ma Xinze <2460798168@qq.com>
2021-02-01 09:42:26 +0800

    A 网关配置

--- codingce-gulimall/gulimall-gateway/src/main/java/cn/com/codingce/gateway/GulimallGatewayApplication.java
+++ codingce-gulimall/gulimall-gateway/src/main/java/cn/com/codingce/gateway/GulimallGatewayApplication.java
@@ -2,8 +2,11 @@ package cn.com.codingce.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GulimallGatewayApplication {
 
     public static void main(String[] args) {
