a742dd3f5b6136b36ed6ef9b7cec7caeae6e5b9f
Ma Xinze <2460798168@qq.com>
2021-02-17 09:09:11 +0800

    A 整合第三方服务

---
+++ codingce-gulimall/gulimall-third-party/src/main/java/cn/com/codingce/thirdparty/GulimallThirdPartyApplication.java
@@ -0,0 +1,13 @@
+package cn.com.codingce.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallThirdPartyApplication.class, args);
+    }
+
+}
