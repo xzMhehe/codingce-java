dfb3c6c965ab205cb87ffd805a3dbfb58e206274
Ma Xinze <2460798168@qq.com>
2021-02-17 17:48:53 +0800

    A 阿里云对象存储后端实现

--- codingce-gulimall/gulimall-product/src/test/java/cn/com/codingce/product/GulimallProductApplicationTests.java
+++ codingce-gulimall/gulimall-product/src/test/java/cn/com/codingce/product/GulimallProductApplicationTests.java
@@ -2,26 +2,15 @@ package cn.com.codingce.product;
 
 import cn.com.codingce.product.entity.BrandEntity;
 import cn.com.codingce.product.service.BrandService;
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.OSSClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
     @Autowired
     private BrandService brandService;
-
-    @Autowired
-    OSSClient ossClient;
-
 
     @Test
     void contextLoads() {
@@ -32,30 +21,4 @@ class GulimallProductApplicationTests {
         brandService.save(brandEntity);
 
     }
-
-    @Test
-    void picOSS() throws FileNotFoundException {
-        // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "endpoint";
-        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "accessKeyId";
-        String accessKeySecret = "accessKeySecret";
-
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-
-        // 上传文件流。
-        InputStream inputStream = new FileInputStream("D:\\comment_bg.png");
-        ossClient.putObject("codingce-product", "comment_bg.png", inputStream);
-
-        // 关闭OSSClient。
-        ossClient.shutdown();
-        System.out.println("上传完成...");
-    }
-
-    @Test
-    void testOssUpload() {
-
-    }
-
 }
--- codingce-gulimall/gulimall-third-party/src/test/java/cn/com/codingce/thirdparty/GulimallThirdPartyApplicationTests.java
+++ codingce-gulimall/gulimall-third-party/src/test/java/cn/com/codingce/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,13 +1,49 @@
 package cn.com.codingce.thirdparty;
 
+import com.aliyun.oss.OSSClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class GulimallThirdPartyApplicationTests {
 
+    @Autowired
+    OSSClient ossClient;
+
     @Test
-    void contextLoads() {
+    void picOSS() throws FileNotFoundException {
+//        // Endpoint以杭州为例，其它Region请按实际情况填写。
+//        String endpoint = "endpoint";
+//        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+//        String accessKeyId = "accessKeyId";
+//        String accessKeySecret = "accessKeySecret";
+//
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//
+//        // 上传文件流。
+//        InputStream inputStream = new FileInputStream("D:\\comment_bg.png");
+//        ossClient.putObject("codingce-product", "comment_bg.png", inputStream);
+//
+//        // 关闭OSSClient。
+//        ossClient.shutdown();
+//        System.out.println("上传完成...");
+    }
+
+    @Test
+    void testOssUpload() throws FileNotFoundException {
+        // 上传文件流。
+        InputStream inputStream = new FileInputStream("D:\\a.png");
+        ossClient.putObject("codingce-product", "a.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传完成...");
     }
 
 }
