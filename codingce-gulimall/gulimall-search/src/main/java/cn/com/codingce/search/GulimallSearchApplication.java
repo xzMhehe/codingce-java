4a5d0b1111674ee59efdf5e01f21ee3edc0492d9
Ma Xinze <2460798168@qq.com>
2021-02-24 14:28:53 +0800

    :fire: 搜索

---
+++ codingce-gulimall/gulimall-search/src/main/java/cn/com/codingce/search/GulimallSearchApplication.java
@@ -0,0 +1,13 @@
+package cn.com.codingce.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallSearchApplication.class, args);
+    }
+
+}
