8f2bdaf80cfcb3b63d1a2453c6d6da54d63ed756
Ma Xinze <2460798168@qq.com>
2021-02-27 14:10:08 +0800

    :art: 完成采购

--- codingce-gulimall/gulimall-ware/src/main/java/cn/com/codingce/ware/dao/WareSkuDao.java
+++ codingce-gulimall/gulimall-ware/src/main/java/cn/com/codingce/ware/dao/WareSkuDao.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 /**
  * 商品库存
- * 
+ *
  * @author mxz
  * @email codingce@gmail.com
  * @date 2021-01-30 09:31:35
@@ -17,11 +17,35 @@ import java.util.List;
 @Mapper
 public interface WareSkuDao extends BaseMapper<WareSkuEntity> {
 
-    Long getSkuStock(@Param("item") Long item);
+    /**
+     * 添加入库信息
+     *
+     * @param skuId
+     * @param wareId
+     * @param skuNum
+     */
+    void addStock(@Param("skuId") Long skuId, @Param("wareId") Long wareId, @Param("skuNum") Integer skuNum);
+
+    Long getSkuStock(@Param("skuId") Long skuId);
 
     List<Long> listWareIdHasSkuStock(@Param("skuId") Long skuId);
 
+    /**
+     * 锁定库存
+     *
+     * @param skuId
+     * @param wareId
+     * @param num
+     * @return
+     */
     Long lockSkuStock(@Param("skuId") Long skuId, @Param("wareId") Long wareId, @Param("num") Integer num);
 
+    /**
+     * 解锁库存
+     *
+     * @param skuId
+     * @param wareId
+     * @param num
+     */
     void unLockStock(@Param("skuId") Long skuId, @Param("wareId") Long wareId, @Param("num") Integer num);
 }
