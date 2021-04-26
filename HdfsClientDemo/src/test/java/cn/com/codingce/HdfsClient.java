2dfa4a81f81ab1bae1c5aee52c3660abe34dd41e
Ma Xinze <2460798168@qq.com>
2021-04-26 11:27:45 +0800

    :fire: HDFS 文件上传(测试参数优先级)

--- HdfsClientDemo/src/test/java/cn/com/codingce/HdfsClient.java
+++ HdfsClientDemo/src/test/java/cn/com/codingce/HdfsClient.java
@@ -17,9 +17,29 @@ public class HdfsClient {
         // 1 获取文件系统
         Configuration configuration = new Configuration();
         // FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"), configuration);
-        FileSystem fs = FileSystem.get(new URI("hdfs://139.9.34.48:8020"), configuration, "codingce");
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"), configuration, "codingce");
         // 2 创建目录
         fs.mkdirs(new Path("/xiyou/huaguoshan/"));
+        // 3 关闭资源
+        fs.close();
+    }
+
+    /**
+     * HDFS 文件上传(测试参数优先级)
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void testCopyFromLocalFile() throws IOException,
+            InterruptedException, URISyntaxException {
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        configuration.set("dfs.replication", "2");
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"),
+                configuration, "codingce"); // 2 上传文件
+        fs.copyFromLocalFile(new Path("/Users/williamma/mxz_code/github/hello.txt"), new Path("/xiyou/huaguoshan"));
         // 3 关闭资源
         fs.close();
     }
