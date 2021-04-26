afbf449f43a8c20c58bd85358768d64b90e4af2f
Ma Xinze <2460798168@qq.com>
2021-04-26 11:34:11 +0800

    :fire: HDFS 文件下载

--- HdfsClientDemo/src/test/java/cn/com/codingce/HdfsClient.java
+++ HdfsClientDemo/src/test/java/cn/com/codingce/HdfsClient.java
@@ -26,6 +26,11 @@ public class HdfsClient {
 
     /**
      * HDFS 文件上传(测试参数优先级)
+     * <p>
+     * 将 hdfs-site.xml 拷贝到项目的 resources 资源目录下
+     * <p>
+     * 参数优先级排序:(1)客户端代码中设置的值 >(2)ClassPath 下的用户自定义配置文
+     * 件 >(3)然后是服务器的自定义配置(xxx-site.xml)>(4)服务器的默认配置(xxx-default.xml)
      *
      * @throws IOException
      * @throws InterruptedException
@@ -40,6 +45,31 @@ public class HdfsClient {
         FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"),
                 configuration, "codingce"); // 2 上传文件
         fs.copyFromLocalFile(new Path("/Users/williamma/mxz_code/github/hello.txt"), new Path("/xiyou/huaguoshan"));
+        // 3 关闭资源
+        fs.close();
+    }
+
+    /**
+     * HDFS 文件下载
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void testCopyToLocalFile() throws IOException,
+            InterruptedException, URISyntaxException {
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"),
+                configuration, "codingce");
+        // 2 执行下载操作
+        // boolean delSrc 指是否将原文件删除
+        // Path src 指要下载的文件路径
+        // Path dst 指将文件下载到的路径
+        // boolean useRawLocalFileSystem 是否开启文件校验 fs.copyToLocalFile(false, new
+        fs.copyToLocalFile(false, new
+                Path("/xiyou/huaguoshan/hello.txt"), new Path("/Users/williamma/mxz_code/github/hello2.txt"), true);
         // 3 关闭资源
         fs.close();
     }
