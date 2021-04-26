21adb978d550f169f19ca1bd991d4cf8c9781770
Ma Xinze <2460798168@qq.com>
2021-04-26 14:10:38 +0800

    :fire: HDFS HDFS 文件详情查看

--- HdfsClientDemo/src/test/java/cn/com/codingce/HdfsClient.java
+++ HdfsClientDemo/src/test/java/cn/com/codingce/HdfsClient.java
@@ -1,13 +1,17 @@
 package cn.com.codingce;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 public class HdfsClient {
 
@@ -110,6 +114,43 @@ public class HdfsClient {
                 configuration, "codingce");
         // 2 执行删除
         fs.delete(new Path("/xiyou"), true);
+        // 3 关闭资源
+        fs.close();
+    }
+
+    /**
+     * HDFS 文件详情查看
+     *
+     * 查看文件名称、权限、长度、块信息
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void testListFiles() throws IOException, InterruptedException,
+            URISyntaxException {
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"),
+                configuration, "codingce");
+        // 2 获取文件详情
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
+        while (listFiles.hasNext()) {
+            LocatedFileStatus fileStatus = listFiles.next();
+            System.out.println("========" + fileStatus.getPath() + "=========");
+            System.out.println(fileStatus.getPermission());
+            System.out.println(fileStatus.getOwner());
+            System.out.println(fileStatus.getGroup());
+            System.out.println(fileStatus.getLen());
+            System.out.println(fileStatus.getModificationTime());
+            System.out.println(fileStatus.getReplication());
+            System.out.println(fileStatus.getBlockSize());
+            System.out.println(fileStatus.getPath().getName());
+            // 获取块信息
+            BlockLocation[] blockLocations = fileStatus.getBlockLocations();
+            System.out.println(Arrays.toString(blockLocations));
+        }
         // 3 关闭资源
         fs.close();
     }
