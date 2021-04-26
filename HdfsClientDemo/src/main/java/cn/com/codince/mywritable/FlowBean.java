e8120c1cfa46e13da0f4e9a17cbac51f0c089163
Ma Xinze <2460798168@qq.com>
2021-04-26 19:19:11 +0800

    :fire: FlowBean

---
+++ HdfsClientDemo/src/main/java/cn/com/codince/mywritable/FlowBean.java
@@ -0,0 +1,36 @@
+package cn.com.codince.mywritable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+//1 继承 Writable 接口
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FlowBean implements Writable {
+
+    private long upFlow; //上行流量
+    private long downFlow; //下行流量
+    private long sumFlow; //总流量
+
+    //4 实现序列化和反序列化方法,注意顺序一定要保持一致
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeLong(upFlow);
+        dataOutput.writeLong(downFlow);
+        dataOutput.writeLong(sumFlow);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.upFlow = dataInput.readLong();
+        this.downFlow = dataInput.readLong();
+        this.sumFlow = dataInput.readLong();
+    }
+}
